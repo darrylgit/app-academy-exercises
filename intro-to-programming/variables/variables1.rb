fe60f4bb653bf661e43b6fc77042f692d1bf1392
darrylgit <darrylsmithup@gmail.com>
2020-05-06 13:10:33 -0400

    Complete first variables exercise

---
+++ intro-to-programming/variables/variables1.rb
@@ -0,0 +1,19 @@
+sentence = "There is no spoon"
+puts "---1:"
+puts sentence + "?"   # There is no spoon?
+
+puts "---2:"
+puts sentence         # There is no spoon
+
+sentence += "."
+puts "---3:"
+puts sentence         # There is no spoon.
+
+num = 6 / 2
+puts "---4:"
+puts num              # 3
+
+puts "---5:"
+puts sentence[num]    # r
+
+# 100% baby!
