f11943f04c6693b17f33b6f09bdbf2d136c9b5a1
darrylgit <darrylsmithup@gmail.com>
2020-05-06 13:13:08 -0400

    Complete the actual first variables exercise

--- intro-to-programming/variables/variables1.rb
+++ intro-to-programming/variables/variables1.rb
@@ -1,19 +1,15 @@
-sentence = "There is no spoon"
-puts "---1:"
-puts sentence + "?"   # There is no spoon?
+# 1. Declare a variable named `firstName` and assign it your first name as a string:
+firstName = "Daniel"
 
-puts "---2:"
-puts sentence         # There is no spoon
 
-sentence += "."
-puts "---3:"
-puts sentence         # There is no spoon.
+# 2. Declare a variable named `lastName` and assign it your last name as a string:
+lastName = "Wagener"
 
-num = 6 / 2
-puts "---4:"
-puts num              # 3
+# 3. Declare a variable named `fullName` and assign it the concatenation of
+# your `firstName` and `lastName`. Be sure to put a space between your
+# first and last names:
+fullName = firstName + " " + lastName
 
-puts "---5:"
-puts sentence[num]    # r
 
-# 100% baby!
+# 4. print out the `fullName` variable
+p fullName
