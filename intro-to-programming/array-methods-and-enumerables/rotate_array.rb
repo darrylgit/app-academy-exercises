f55bc34d085b29d97101ec2bf78e89984f9aeb8c
darrylgit <darrylsmithup@gmail.com>
2020-05-08 10:25:28 -0400

    Complete Rotate Array

---
+++ intro-to-programming/array-methods-and-enumerables/rotate_array.rb
@@ -0,0 +1,10 @@
+def rotate_array(arr, num)
+  num.times { arr.unshift(arr.pop) }
+  return arr
+end
+
+print rotate_array([ "Matt", "Danny", "Mashu", "Matthias" ], 1) # => [ "Matthias", "Matt", "Danny", "Mashu" ]
+puts
+
+print rotate_array([ "a", "b", "c", "d" ], 2) # => [ "c", "d", "a", "b" ]
+puts
