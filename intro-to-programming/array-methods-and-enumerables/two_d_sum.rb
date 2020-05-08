31c2c7346230e304769675438a7d1983bfa7caf4
darrylgit <darrylsmithup@gmail.com>
2020-05-08 11:30:57 -0400

    Complete Two D Sum

---
+++ intro-to-programming/array-methods-and-enumerables/two_d_sum.rb
@@ -0,0 +1,20 @@
+def two_d_sum(arr)
+  count = 0
+
+  arr.each { |array| array.each { |num| count += num }}
+
+  return count
+end
+
+array_1 = [
+  [4, 5],
+  [1, 3, 7, 1]
+]
+puts two_d_sum(array_1)    # => 21
+
+array_2 = [
+  [3, 3],
+  [2],
+  [2, 5]
+]
+puts two_d_sum(array_2)    # => 15
