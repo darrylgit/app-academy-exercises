cfe139046d836d585d80cb1308130a04ed956bc4
darrylgit <darrylsmithup@gmail.com>
2020-05-08 11:34:31 -0400

    Complete Two D Translate

---
+++ intro-to-programming/array-methods-and-enumerables/two_d_translate.rb
@@ -0,0 +1,28 @@
+def two_d_translate(arr)
+  outputArray = []
+
+  arr.each do |innerArray|
+    innerArray[1].times do
+      outputArray << innerArray[0]
+    end
+  end
+
+  return outputArray
+end
+
+arr_1 = [
+  ['boot', 3],
+  ['camp', 2],
+  ['program', 0]
+]
+
+print two_d_translate(arr_1) # => [ 'boot', 'boot', 'boot', 'camp', 'camp' ]
+puts
+
+arr_2 = [
+  ['red', 1],
+  ['blue', 4]
+]
+
+print two_d_translate(arr_2) # => [ 'red', 'blue', 'blue', 'blue', 'blue' ]
+puts
