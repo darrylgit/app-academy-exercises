10f0cfa89fc1fb4456cfe70660315f25d759f5b3
darrylgit <darrylsmithup@gmail.com>
2020-05-09 10:33:32 -0400

    Complete Element Replace

---
+++ intro-to-programming/hashes/element_replace.rb
@@ -0,0 +1,19 @@
+def element_replace(arr, hash)
+  arr.each_with_index  do |el, i|
+    if hash.has_key?(el)
+      arr[i] = hash[el]
+    end
+  end
+
+  return arr
+end
+
+arr1 = ["LeBron James", "Lionel Messi", "Serena Williams"]
+hash1 = {"Serena Williams"=>"tennis", "LeBron James"=>"basketball"}
+print element_replace(arr1, hash1) # => ["basketball", "Lionel Messi", "tennis"]
+puts
+
+arr2 = ["dog", "cat", "mouse"]
+hash2 = {"dog"=>"bork", "cat"=>"meow", "duck"=>"quack"}
+print element_replace(arr2, hash2) # => ["bork", "meow", "mouse"]
+puts
