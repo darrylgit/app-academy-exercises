72ab180e4a6516f1789343fe6b23b9843eeff4d0
darrylgit <darrylsmithup@gmail.com>
2020-05-09 21:26:16 -0400

    Complete Caesar Cipher

---
+++ intro-to-programming/advanced-problems/caesar_cipher.rb
@@ -0,0 +1,14 @@
+# Feel free to use this variable:
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+def caesar_cipher(str, num)
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+  str.each_char.with_index { |letter, i| str[i] = alphabet[(alphabet.index(letter) + num) % 26] }
+
+  return str
+end
+
+puts caesar_cipher("apple", 1)    #=> "bqqmf"
+puts caesar_cipher("bootcamp", 2) #=> "dqqvecor"
+puts caesar_cipher("zebra", 4)    #=> "difve"
