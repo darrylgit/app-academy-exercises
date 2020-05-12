530f9cd74ff58e488bf665475c15ec1979ed16ba
darrylgit <darrylsmithup@gmail.com>
2020-05-12 11:03:38 -0400

    Complete Only Vowels

--- software-engineering-foundations/advanced_methods_exercise/03_only_vowels.rb
+++ software-engineering-foundations/advanced_methods_exercise/03_only_vowels.rb
@@ -1,7 +1,9 @@
 # Write a method, `only_vowels?(str)`, that accepts a string as an arg.
 # The method should return true if the string contains only vowels.
 # The method should return false otherwise.
-
+def only_vowels?(str)
+  str.split('').all? {|char| "aeiou".include?(char )}
+end
 
 p only_vowels?("aaoeee")  # => true
 p only_vowels?("iou")     # => true
