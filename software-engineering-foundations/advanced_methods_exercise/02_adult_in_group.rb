874b26262e6663ba7ede46bbd1443b28bd38ff4a
darrylgit <darrylsmithup@gmail.com>
2020-05-12 11:01:18 -0400

    Complete Adult in Group

--- software-engineering-foundations/advanced_methods_exercise/02_adult_in_group.rb
+++ software-engineering-foundations/advanced_methods_exercise/02_adult_in_group.rb
@@ -2,6 +2,10 @@
 # The method should return true if there is at least 1 person with an age of 18 or greater.
 # The method should return false otherwise.
 
+def adult_in_group?(people) 
+    people.any? { |person| person[:age] >= 18}
+end
+
 
 people_1 = [
     {name: "Jack", age: 17},
