a660688d77845c88bd0b497dfdcd4b99f87e2e4e
darrylgit <darrylsmithup@gmail.com>
2020-05-12 11:15:35 -0400

    Complete Flatten

--- software-engineering-foundations/advanced_methods_exercise/06_union.rb
+++ software-engineering-foundations/advanced_methods_exercise/06_union.rb
@@ -1,5 +1,8 @@
 # Write a method, union, that accepts any number of arrays as arguments.
 # The method should return an array containing all elements of the given arrays.
+def union(*arrays)
+  arrays.flatten
+end
 
 
 p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
