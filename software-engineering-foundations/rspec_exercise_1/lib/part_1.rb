2f2e4d50daa075db3ed8120358c97424d47cf3e1
darrylgit <darrylsmithup@gmail.com>
2020-05-14 17:22:58 -0400

    Complete rspec pt. 1

--- software-engineering-foundations/rspec_exercise_1/lib/part_1.rb
+++ software-engineering-foundations/rspec_exercise_1/lib/part_1.rb
@@ -1,9 +1,21 @@
 def average(num1, num2)
-  return (num1 + num2) / 2.0
+  (num1 + num2) / 2.0
 end
 
 def average_array(arr)
-  return arr.sum / ( arr.length * 1.0 )
+  arr.sum / ( arr.length * 1.0 )
 end
 
 puts average_array([1, 2, 3])
+
+def repeat(str, num)
+  str * num
+end
+
+def yell(str)
+  str.upcase + "!"
+end
+
+def alternating_case(str)
+  str.split.map.with_index { |word, i| i % 2 == 0 ? word.upcase  : word.downcase }.join(' ')
+end
