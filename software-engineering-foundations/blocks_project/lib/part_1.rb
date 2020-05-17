50f679d4390c1f7aad377310dca9ddcd96b3db83
darrylgit <darrylsmithup@gmail.com>
2020-05-17 10:41:59 -0400

    Complete blocks project pt. 1

--- software-engineering-foundations/blocks_project/lib/part_1.rb
+++ software-engineering-foundations/blocks_project/lib/part_1.rb
@@ -5,3 +5,15 @@ end
 def reject_puppies(dogsArr)
   dogsArr.reject { |dog| dog["age"] <= 2}
 end
+
+def count_positive_subarrays(arr)
+  arr.count { |subArr| subArr.sum > 0 }
+end
+
+def aba_translate(str)
+  str.split("").map { |char| "aeiou".include?(char) ? char + "b" + char : char }.join("")
+end
+
+def aba_array(arr)
+  arr.map { |el| aba_translate(el) }
+end
