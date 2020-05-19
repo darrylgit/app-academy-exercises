630320178b71c8d3712fc4a4afd1f2543fb79b57
darrylgit <darrylsmithup@gmail.com>
2020-05-19 13:16:29 -0400

    Complete Hangman project pt. 1

---
+++ software-engineering-foundations/hangman_project/lib/hangman.rb
@@ -0,0 +1,41 @@
+class Hangman
+  DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+
+  def self.random_word
+    DICTIONARY.sample
+  end
+
+  def initialize
+    @secret_word = self.class.random_word
+    @guess_word = Array.new(@secret_word.length, "_")
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
+
+  def guess_word
+    @guess_word
+  end
+
+  def attempted_chars
+    @attempted_chars
+  end
+
+  def remaining_incorrect_guesses
+    @remaining_incorrect_guesses
+  end
+
+  def already_attempted?(char)
+    @attempted_chars.include?(char)
+  end
+
+  def get_matching_indices(char)
+    (0...@secret_word.length).select { |idx| @secret_word[idx] == char }
+  end
+
+  def fill_indices(char, arr)
+    arr.each { |idx| @guess_word[idx] = char }
+  end
+
+
+
+end
