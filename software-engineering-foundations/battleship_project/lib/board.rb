bdeb7423894f0f5e6044153563ad04be9bf3b38e
darrylgit <darrylsmithup@gmail.com>
2020-05-23 16:37:29 -0400

    Complete Battleship project pt. 2

--- software-engineering-foundations/battleship_project/lib/board.rb
+++ software-engineering-foundations/battleship_project/lib/board.rb
@@ -1,5 +1,11 @@
 class Board
 
+  # Class method(s)
+  def self.print_grid(grid)
+    grid.each {|row| puts row.join(" ")}
+  end
+
+  # Init, reader(s)
   attr_reader :size
 
   def initialize(n)
@@ -7,6 +13,7 @@ class Board
     @size = n * n
   end
 
+  # Instance methods
   def [](coordinates)
     @grid[coordinates[0]][coordinates[1]]
   end
@@ -16,7 +23,45 @@ class Board
   end
 
   def num_ships
-    @grid.inject(0) { |total, row| total + row.count { |el| el == :S } }
+    @grid.inject(0) { |total, row| total + row.count { |square| square == :S } }
+  end
+
+  def attack(coordinates)
+    if self.[](coordinates) == :S 
+      self.[]=(coordinates, :H)
+      puts "you sunk my battleship"
+      return true
+    end
+
+    self.[]=(coordinates, :X)
+    false
+  end
+
+  def place_random_ships
+    quota = @size / 4
+    
+    while self.num_ships < quota
+      @grid.each do |row|
+        row.each_with_index do |square, i|
+          row[i] = :S if rand(0...4) == 0
+          return if self.num_ships == quota
+        end
+      end
+    end
+  end
+
+  def hidden_ships_grid
+    @grid.map do |row|
+      row.map { |square| square == :S ? :N : square }
+    end
+  end
+
+  def cheat
+    Board.print_grid(@grid)
+  end
+
+  def print
+    Board.print_grid(self.hidden_ships_grid)
   end
 
 end
