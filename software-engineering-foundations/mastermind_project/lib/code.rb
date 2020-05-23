d63e5a27553861d5973440d627db3a352b3c54d5
darrylgit <darrylsmithup@gmail.com>
2020-05-23 12:50:11 -0400

    Complete Mastermind code spec  pt. 2

--- software-engineering-foundations/mastermind_project/lib/code.rb
+++ software-engineering-foundations/mastermind_project/lib/code.rb
@@ -38,4 +38,31 @@ class Code
     @pegs.length
   end
 
+  def num_exact_matches(guess)
+    # Fancy-pants one-liner!
+    # Patching the optional index argument available in Javascript's Array.reduce()
+    guess.pegs.each_with_index.inject(0) { |acc, (peg, i)| peg == @pegs[i] ? acc + 1 : acc }
+
+    # Alternate, non-inject route:
+    # tally = 0
+    # guess.pegs.each_with_index { |peg, i| tally += 1 if @pegs[i] == peg }
+    # tally
+  end
+
+  def num_near_matches(guess)
+    tally = 0
+    guess.pegs.each_with_index { |peg, i| tally += 1 if @pegs.include?(peg) && @pegs[i] != peg }
+    tally
+
+    # I love reducing arrays, but it's not always the way to go:
+
+    # total_right = guess.pegs.inject(0) { |acc, peg| @pegs.include?(peg) ? acc + 1 : acc }
+    # exact_matches = self.num_exact_matches(guess)
+    # total_right - exact_matches
+  end
+
+  def ==(code)
+    code.pegs == @pegs
+  end
+
 end
