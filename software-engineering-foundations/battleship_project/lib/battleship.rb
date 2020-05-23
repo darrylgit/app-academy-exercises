8ba36cff68e7b3d3c2d85d0d6fd719b9873914f0
darrylgit <darrylsmithup@gmail.com>
2020-05-23 17:15:42 -0400

    Complete Battleship project

--- software-engineering-foundations/battleship_project/lib/battleship.rb
+++ software-engineering-foundations/battleship_project/lib/battleship.rb
@@ -3,4 +3,45 @@ require_relative "player"
 
 class Battleship
 
+  # Init, readers
+  attr_reader :board, :player
+
+  def initialize(n)
+    @player = Player.new()
+    @board = Board.new(n)
+    @remaining_misses = @board.size / 2
+  end
+
+  # Instance methods
+  def start_game
+    @board.place_random_ships
+    puts @board.num_ships
+    @board.print
+  end
+
+  def lose?
+    return false if @remaining_misses > 0
+
+    puts "you lose"
+    true
+  end
+
+  def win?
+    return false if @board.num_ships > 0
+
+    puts "you win"
+    true
+  end
+
+  def game_over?
+    self.lose? || self.win?
+  end
+
+  def turn
+    coordinates = @player.get_move
+    @remaining_misses -= 1 if !@board.attack(coordinates)
+    @board.print
+    puts "Remaining misses: #{@remaining_misses}"
+  end
+
 end
--- software-engineering-foundations/battleship_project/lib/player.rb
+++ software-engineering-foundations/battleship_project/lib/player.rb
@@ -1,3 +1,7 @@
 class Player
-
+  def get_move
+    puts "enter a position with coordinates separated with a space like `4 7`"
+    input = gets.chomp
+    input.split(" ").map(&:to_i)
+  end
 end
