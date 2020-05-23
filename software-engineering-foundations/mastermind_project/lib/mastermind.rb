bc323ba847c3a353a7f6c88818e2bcb0d3f8c387
darrylgit <darrylsmithup@gmail.com>
2020-05-23 14:03:31 -0400

    Complete Mastermind project

--- software-engineering-foundations/mastermind_project/lib/mastermind.rb
+++ software-engineering-foundations/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,21 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+    @secret_code = Code.new(Code.random(length).pegs)
+  end
 
+  def print_matches(code)
+    puts "Exact matches: #{@secret_code.num_exact_matches(code)}"
+    puts "Near matches: #{@secret_code.num_near_matches(code)}"
+  end
+
+  def ask_user_for_guess
+    puts "Enter a code: "
+    guess = Code.from_string(gets.chomp)
+
+    self.print_matches(guess)
+
+    @secret_code == guess
+  end
 end
