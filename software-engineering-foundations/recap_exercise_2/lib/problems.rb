9705dbe074fef48c9231c577983c58740e1d00b4
darrylgit <darrylsmithup@gmail.com>
2020-05-26 09:57:58 -0400

    Complete recap exercise 2

---
+++ software-engineering-foundations/recap_exercise_2/lib/problems.rb
@@ -0,0 +1,84 @@
+# Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple
+# of both of the given numbers
+def least_common_multiple(num_1, num_2)
+    i = [num_1, num_2].max
+
+    while i <= num_1 * num_2
+        return i if i % num_1 == 0 && i % num_2 == 0
+        i += 1
+    end
+end
+
+
+# Write a method, most_frequent_bigram, that takes in a string and returns the two adjacent letters that appear the
+# most in the string.
+def most_frequent_bigram(str)
+    bigrams = Hash.new(0)
+    (0...str.length - 1).each { |i| bigrams[str[i] + str[i + 1]] += 1 }
+    
+    bigrams.keys.inject() do |acc, bigram|
+        if !acc
+            bigram
+        end
+
+        bigrams[acc] < bigrams[bigram] ? bigram : acc
+    end
+end
+
+
+class Hash
+    # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
+    def inverse
+        output = {}
+        self.each { |k, v| output[v] = k}
+        output
+    end
+end
+
+
+class Array
+    # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
+    def pair_sum_count(num)
+        # Lol jeez. Again, promise I didn't copy this one, Alvin's just rubbing off on me apparently
+        count = 0
+        (0...self.length).each do |idx1|
+            (idx1 + 1...self.length).each do |idx2|
+                count += 1 if self[idx1] + self[idx2] == num
+            end
+        end
+
+        count
+    end
+
+    # Write a method, Array#bubble_sort, that takes in an optional proc argument.
+    # When given a proc, the method should sort the array according to the proc.
+    # When no proc is given, the method should sort the array in increasing order.
+    #
+    # Sorting algorithms like bubble_sort, commonly accept a block. That block accepts
+    # two parameters, which represents the two elements in the array being compared.
+    # If the block returns 1, it means that the second element passed to the block
+    # should go before the first (i.e. switch the elements). If the block returns -1,
+    # it means the first element passed to the block should go before the second
+    # (i.e. do not switch them). If the block returns 0 it implies that
+    # it does not matter which element goes first (i.e. do nothing).
+    #
+    # This should remind you of the spaceship operator! Convenient :)
+    def bubble_sort(&prc)
+        prc ||= Proc.new { |el, next_el| el <=> next_el }
+
+        sorted = false
+
+        while !sorted
+            sorted = true
+            (0...self.length - 1).each do |i|
+            
+                if prc.call(self[i], self[i + 1]) == 1
+                    sorted = false
+                    self[i], self[i + 1] = self[i + 1], self[i]
+                end
+            end
+        end
+
+        self
+    end
+end
