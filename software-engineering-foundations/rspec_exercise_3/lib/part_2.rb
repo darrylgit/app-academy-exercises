c85f24c141d77910e8f8b718e12f074ea4934f88
darrylgit <darrylsmithup@gmail.com>
2020-06-19 13:18:59 -0400

    Complete char_replace!

--- software-engineering-foundations/rspec_exercise_3/lib/part_2.rb
+++ software-engineering-foundations/rspec_exercise_3/lib/part_2.rb
@@ -3,3 +3,7 @@ def element_count(arr)
   arr.each { |el| counts[el] += 1 }
   counts
 end
+
+def char_replace!(str, hash)
+  str.each_char.with_index { |char, i| hash.has_key?(char) ? str[i] = hash[char] : nil }
+end
