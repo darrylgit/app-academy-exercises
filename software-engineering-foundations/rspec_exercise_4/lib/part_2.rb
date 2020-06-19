6535b7cb7fa3c5940f15fc97f86370c8f0cdd1b0
darrylgit <darrylsmithup@gmail.com>
2020-06-19 14:16:39 -0400

    Complete respec exercise 4

--- software-engineering-foundations/rspec_exercise_4/lib/part_2.rb
+++ software-engineering-foundations/rspec_exercise_4/lib/part_2.rb
@@ -1,3 +1,27 @@
 def proper_factors(num)
   (1...num).select { |factor| num % factor == 0}
 end
+
+def aliquot_sum(num)
+  proper_factors(num).sum 
+end
+
+def perfect_number?(num)
+  aliquot_sum(num) == num
+end
+
+def ideal_numbers(num)
+  output = []
+
+  while output.length < num
+    candidate = output[-1] ? output[-1] + 1 : 6
+
+    while !perfect_number?(candidate)
+      candidate += 1
+    end
+
+    output << candidate
+  end
+
+  output
+end
