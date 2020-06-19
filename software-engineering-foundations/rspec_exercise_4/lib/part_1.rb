60ecdbf2f70c5f6f508301a93df7008c03f23fde
darrylgit <darrylsmithup@gmail.com>
2020-06-19 13:34:33 -0400

    Complete got_one?

--- software-engineering-foundations/rspec_exercise_4/lib/part_1.rb
+++ software-engineering-foundations/rspec_exercise_4/lib/part_1.rb
@@ -2,3 +2,17 @@ def my_reject(arr, &prc)
   arr.select { |el| !prc.call(el) }
 end
 
+def my_one?(arr, &prc)
+  got_one = false;
+  arr.each do |el|
+    if prc.call(el)
+      if !got_one
+        got_one = true
+      elsif got_one
+        return false
+      end
+    end
+  end
+
+  return got_one
+end
