82beac051e7c0199de4b24d4ca4e10cfcd2628ce
darrylgit <darrylsmithup@gmail.com>
2020-06-21 21:15:46 -0400

    Complete zany_zip

--- software-engineering-foundations/rspec_exercise_5/lib/exercise.rb
+++ software-engineering-foundations/rspec_exercise_5/lib/exercise.rb
@@ -14,3 +14,23 @@ end
 def prizz_proc(arr, prc1, prc2)
   arr.select { |el| [prc1.call(el), prc2.call(el)].one? }
 end
+
+def zany_zip(*arrs)
+  max_length = arrs.map { |arr| arr.length}.max;
+
+  output = Array.new(max_length) { [] }
+
+  arrs.each do |arr|
+    while arr.length < max_length
+      arr << nil
+    end
+  end
+
+  arrs.each do |arr|
+    arr.each_with_index do |el, i|
+      output[i] << el
+    end
+  end
+
+  output
+end
