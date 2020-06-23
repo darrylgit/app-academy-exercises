9e6a1eee1a79a2926006c9704e0b3e902e332bcb
darrylgit <darrylsmithup@gmail.com>
2020-06-23 13:22:01 -0400

    Complete matrix_addition

--- software-engineering-foundations/nauseating_numbers/lib/phase_2.rb
+++ software-engineering-foundations/nauseating_numbers/lib/phase_2.rb
@@ -5,3 +5,11 @@ end
 def divisor_count(num)
   (1...num).inject(0) { |acc, factor| num % factor == 0 ? acc + 1 : acc }
 end
+
+def matrix_addition(matrix1, matrix2)
+  matrix1.each_with_index.map do |arr, i|
+    arr.each_with_index.map do |el, j|
+      el += matrix2[i][j]
+    end
+  end
+end
--- software-engineering-foundations/nauseating_numbers/spec/phase_2_spec.rb
+++ software-engineering-foundations/nauseating_numbers/spec/phase_2_spec.rb
@@ -14,3 +14,18 @@ describe "anti_prime?" do
     expect(anti_prime?(1024)).to eq(false) 
   end
 end
+
+describe "matrix addition" do
+  let(:matrix_a) { [[2,5], [4,7]] }
+  let(:matrix_b) { [[9,1], [3,0]] }
+  let(:matrix_c) { [[-1,0], [0,-1]] }
+  let(:matrix_d) { [[2, -5], [7, 10], [0, 1]] }
+  let(:matrix_e) { [[0 , 0], [12, 4], [6,  3]] }
+
+  it "should add matrices, idk man" do
+    expect(matrix_addition(matrix_a, matrix_b)).to eq([[11, 6], [7, 7]])  
+    expect(matrix_addition(matrix_a, matrix_c)).to eq([[1, 5], [4, 6]])  
+    expect(matrix_addition(matrix_b, matrix_c)).to eq([[8, 1], [3, -1]])  
+    expect(matrix_addition(matrix_d, matrix_e)).to eq([[2, -5], [19, 14], [6, 4]]) 
+  end
+end
