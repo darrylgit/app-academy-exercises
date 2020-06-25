673a31103af45e7de8c63ad8973cfac520ce3e75
darrylgit <darrylsmithup@gmail.com>
2020-06-25 13:11:28 -0400

    Complete filter_out!

--- software-engineering-foundations/perilous_procs/lib/phase_2.rb
+++ software-engineering-foundations/perilous_procs/lib/phase_2.rb
@@ -1,3 +1,15 @@
 def xnor_select(arr, prc1, prc2)
   arr.select { |el| prc1.call(el) == prc2.call(el) }
 end
+
+def filter_out!(arr, &prc)
+  filtered = arr.select { |el| !prc.call(el) }
+
+  while arr.length > 0
+    arr.pop
+  end
+
+  while filtered.length > 0
+    arr.push(filtered.shift)
+  end
+end
--- software-engineering-foundations/perilous_procs/spec/phase_2_spec.rb
+++ software-engineering-foundations/perilous_procs/spec/phase_2_spec.rb
@@ -11,3 +11,23 @@ describe "xnor_select" do
     expect(xnor_select([-7, -13, 12, 5, -10], is_odd, is_positive)).to eq([5, -10])
   end
 end
+
+describe "filter_out!" do
+    let(:arr_1) { [10, 6, 3, 2, 5] }
+    let(:arr_2) { [1, 7, 3, 5] }
+    let(:arr_3) { [10, 6, 3, 2, 5] }
+    let(:arr_4) { [1, 7, 3, 5] }
+
+  it "removes elements of the input array that return true when given to the block" do
+    
+    filter_out!(arr_1) { |x| x.odd? }
+    filter_out!(arr_2) { |x| x.odd? }
+    filter_out!(arr_3) { |x| x.even? }
+    filter_out!(arr_4) { |x| x.even? }
+
+    expect(arr_1).to eq([10, 6, 2])
+    expect(arr_2).to eq([])
+    expect(arr_3).to eq([3, 5])
+    expect(arr_4).to eq([1, 7, 3, 5])
+  end
+end
