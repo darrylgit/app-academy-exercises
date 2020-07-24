93bd7a2ff8cc106e958d83a31c64bf308a838d71
darrylgit <darrylsmithup@gmail.com>
2020-07-24 15:32:25 -0400

    Finish class initialization and accessors

--- software-engineering-foundations/todo_board_project/item.rb
+++ software-engineering-foundations/todo_board_project/item.rb
@@ -9,4 +9,21 @@ class Item
 
     true
   end
+
+  attr_accessor :title, :description
+  attr_reader :deadline
+
+  def initialize(title, deadline, description)
+    raise "Invalid date" if !self.class.valid_date?(deadline)
+
+    @title = title;
+    @deadline = deadline;
+    @description = description
+  end
+
+  def deadline=(new_deadline)
+    raise "Invalid date" if !self.class.valid_date?(new_deadline)
+
+    @deadline = new_deadline
+  end
 end
