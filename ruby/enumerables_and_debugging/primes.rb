8aee169ce5f41495fe738179baf153b83e8a03ed
darrylgit <darrylsmithup@gmail.com>
2020-09-10 13:50:30 -0400

    Finish debugging exercise

--- ruby/enumerables_and_debugging/primes.rb
+++ ruby/enumerables_and_debugging/primes.rb
@@ -1,19 +1,24 @@
-# primes.rb
+require 'byebug'
 
 def prime?(num)
-  (1..num).each do |idx|
+  (2...num).each do |idx|
     if num % idx == 0
       return false
     end
   end
+
+  true
 end
 
 def primes(num_primes)
   ps = []
-  num = 1
+  num = 2
   while ps.count < num_primes
-    primes << num if prime?(num)
+    ps << num if prime?(num)
+    num += 1
   end
+
+  ps
 end
 
 if __FILE__ == $PROGRAM_NAME
