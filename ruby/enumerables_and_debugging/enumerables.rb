af4189fbae4ec30226b5d0b45682ed9377e982c3
darrylgit <darrylsmithup@gmail.com>
2020-09-10 17:34:56 -0400

    Create my_flatten

--- ruby/enumerables_and_debugging/enumerables.rb
+++ ruby/enumerables_and_debugging/enumerables.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Array 
   def my_each(&prc)
     idx = 0
@@ -37,5 +39,21 @@ class Array
   def my_all?(&prc)
     self.my_each { |el| return false if !prc.call(el)}
     true
+  end
+
+  def my_flatten()
+    return self if self.length == 1 && !self[0].is_a?(Array)
+    
+    first_element = self[0]
+    if first_element.is_a?(Array)
+      first_element = first_element.my_flatten
+    end
+
+    elements_after_first = self[1..-1]
+    if elements_after_first.is_a?(Array)
+      elements_after_first = elements_after_first.my_flatten
+    end
+
+    [*first_element, *elements_after_first]
   end
 end
