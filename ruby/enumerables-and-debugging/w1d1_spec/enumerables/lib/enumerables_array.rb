be07d17a9452bb80008a36826c982924f64d22c5
darrylgit <darrylsmithup@gmail.com>
2020-09-11 10:31:14 -0400

    Complete factors

---
+++ ruby/enumerables-and-debugging/w1d1_spec/enumerables/lib/enumerables_array.rb
@@ -0,0 +1,34 @@
+def factors(num)
+  output = []
+  i = 1
+
+  while i <= num
+    if (num % i == 0)
+      output << i
+    end
+    i += 1
+  end
+
+  return output
+end
+
+class Array
+  def bubble_sort(&prc)
+    prc ||= Proc.new { |el, next_el| el <=> next_el }
+
+    sorted = false
+
+    while !sorted
+        sorted = true
+        (0...self.length - 1).each do |i|
+        
+            if prc.call(self[i], self[i + 1]) == 1
+                sorted = false
+                self[i], self[i + 1] = self[i + 1], self[i]
+            end
+        end
+    end
+
+    self
+  end
+end
