a8175634fc436e25276919b131e451d06925b7e7
darrylgit <darrylsmithup@gmail.com>
2020-09-19 13:40:40 -0400

    Hide zero values in board render

--- ruby/reference/sudoku/tile.rb
+++ ruby/reference/sudoku/tile.rb
@@ -1,9 +1,10 @@
 require 'colorize'
 
 class Tile 
-  def initialize(value, given = false)
-    @value = value
-    @given = given
+  def initialize(value)
+    @value = value.to_i
+
+    @given = @value == 0 
   end
 
   def update_value(val)
@@ -11,6 +12,9 @@ class Tile
   end
 
   def to_s
-    " " + (@given ? @value.to_s.colorize(:light_blue) : @value.to_s) + " "
+    # Print a space if value is zero
+    value_to_print = @value == 0 ? " " : @value.to_s
+    
+    " " + (@given ? value_to_print.colorize(:light_blue) : value_to_print) + " "
   end
 end
