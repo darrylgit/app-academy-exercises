cd975e71e7aec3fd47ed5d5877b4c20d0a16fbe9
darrylgit <darrylsmithup@gmail.com>
2020-09-24 16:06:16 -0400

    Complete both array sum exercises

--- ruby/recursion/excercises/lib/exercises.rb
+++ ruby/recursion/excercises/lib/exercises.rb
@@ -3,3 +3,13 @@ def range(start_val, end_val)
 
   [start_val] + range(start_val + 1, end_val)
 end
+
+def array_sum_recursive(arr)
+  return arr[0] if arr.length == 1
+
+  arr[0] + array_sum_recursive(arr[1..-1])
+end
+
+def array_sum_iterative(arr)
+  arr.inject() { |acc, val| acc + val }
+end
--- ruby/recursion/excercises/spec/exercises_spec.rb
+++ ruby/recursion/excercises/spec/exercises_spec.rb
@@ -6,3 +6,19 @@ describe "range" do
     expect(range(5, 1)).to eq([])
   end
 end
+
+describe "array_sum_recursive" do
+  it "sums all values in array" do
+    expect(array_sum_recursive([1, 2, 3, 4])).to eq(10)
+    expect(array_sum_recursive([1, 2, 3])).to eq(6)
+    expect(array_sum_recursive([1])).to eq(1)
+  end
+end
+
+describe "array_sum_iterative" do
+  it "sums all values in array" do
+    expect(array_sum_iterative([1, 2, 3, 4])).to eq(10)
+    expect(array_sum_iterative([1, 2, 3])).to eq(6)
+    expect(array_sum_iterative([1])).to eq(1)
+  end
+end
