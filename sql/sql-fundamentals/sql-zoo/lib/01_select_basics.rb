95a1aca90e9a6fe50c6768a1bd8fd962f95ee2b8
darrylgit <darrylsmithup@gmail.com>
2020-10-30 16:12:38 -0400

    Complete SQLZOO no.1

--- sql/sql-fundamentals/sql-zoo/lib/01_select_basics.rb
+++ sql/sql-fundamentals/sql-zoo/lib/01_select_basics.rb
@@ -49,6 +49,12 @@ def small_and_wealthy
   # Show the name and continent of countries where the area is less than 2,000
   # and the gdp is more than 5,000,000,000.
   execute(<<-SQL)
+  SELECT
+    name, continent
+  FROM
+    countries
+  WHERE
+    area < 2000 AND gdp > 5000000000
   SQL
 end
 
@@ -56,12 +62,24 @@ def scandinavia
   # Show the name and the population for 'Denmark', 'Finland', 'Norway', and
   # 'Sweden'
   execute(<<-SQL)
+  SELECT 
+    name, population
+  FROM 
+    countries
+  WHERE 
+    name = 'Denmark' OR name = 'Finland' OR name = 'Norway' OR name = 'Sweden';
   SQL
 end
 
 def starts_with_g
   # Show each country that begins with the letter G
   execute(<<-SQL)
+  SELECT 
+    name
+  FROM 
+    countries
+  WHERE
+    name LIKE 'G%'
   SQL
 end
 
@@ -70,5 +88,11 @@ def just_the_right_size
   # with an area between 200,000 and 250,000.
   # BETWEEN allows range checking - note that it is inclusive.
   execute(<<-SQL)
+  SELECT 
+    name, area / 1000
+  FROM 
+    countries
+  WHERE 
+    area BETWEEN  200000 AND 250000
   SQL
 end
