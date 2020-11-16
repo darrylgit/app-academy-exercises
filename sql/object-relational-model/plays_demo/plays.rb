1c9f5a14f0521ef01c36f611da0a9185e7796269
darrylgit <darrylsmithup@gmail.com>
2020-11-16 08:35:33 -0500

    Define Playwright class and Playwright::all

--- sql/object-relational-model/plays_demo/plays.rb
+++ sql/object-relational-model/plays_demo/plays.rb
@@ -79,3 +79,16 @@ class Play
   end
 
 end
+
+class Playwright 
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) } 
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+end
