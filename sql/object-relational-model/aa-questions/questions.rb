dde754e4c6abaa02fbbbacf8c0f0f0f6f7cb2e71
darrylgit <darrylsmithup@gmail.com>
2020-11-24 22:39:44 -0500

    Scaffoled QuestionLike class

--- sql/object-relational-model/aa-questions/questions.rb
+++ sql/object-relational-model/aa-questions/questions.rb
@@ -96,3 +96,60 @@ class QuestionFollow
   end
 end
 
+class Reply
+  attr_accessor :id, :parent_reply, :body, :author, :question_id
+
+  def self.all
+    data = QuestionsDBConnection.instance.execute("SELECT * FROM replies")
+    data.map { |datum| Reply.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDBConnection.instance.execute(<<-SQL, id)
+      SELECT 
+        *
+      FROM
+        replies
+      WHERE 
+        id = ?
+    SQL
+
+    Reply.new(data.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @parent_reply = options['parent_reply']
+    @body = options['body']
+    @author = options['author']
+    @question_id = options['question_id']
+  end
+end
+
+class QuestionLike
+  attr_accessor :id, :question_id, :user_id
+
+  def self.all
+    data = QuestionsDBConnection.instance.execute("SELECT * FROM question_likes")
+    data.map { |datum| QuestionLike.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDBConnection.instance.execute(<<-SQL, id)
+      SELECT 
+        *
+      FROM
+        question_likes
+      WHERE 
+        id = ?
+    SQL
+
+    QuestionLike.new(data.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @user_id = options['user_id']
+  end
+end
