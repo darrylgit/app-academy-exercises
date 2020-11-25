64b4e3bffe8eb7a8d3d18ba72b7c26c98dc4b9f9
darrylgit <darrylsmithup@gmail.com>
2020-11-24 22:30:54 -0500

    Scaffold User class

--- sql/object-relational-model/aa-questions/questions.rb
+++ sql/object-relational-model/aa-questions/questions.rb
@@ -10,3 +10,31 @@ class QuestionsDBConnection < SQLite3::Database
     self.results_as_hash = true
   end
 end
+
+class User
+  attr_accessor :id, :fname, :lname
+
+  def self.all
+    data = QuestionsDBConnection.instance.execute("SELECT * FROM users")
+    # data.map { |datum| User.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    data = QuestionsDBConnection.instance.execute(<<-SQL, id)
+      SELECT 
+        *
+      FROM
+        users
+      WHERE 
+        id = ?
+    SQL
+
+    User.new(data.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+end
