4538b59d184dcc1ad581fe9d9a10a7f98479b946
darrylgit <darrylsmithup@gmail.com>
2020-11-25 20:53:19 -0500

    Define Reply:find_by_question_id

--- sql/object-relational-model/aa-questions/reply.rb
+++ sql/object-relational-model/aa-questions/reply.rb
@@ -19,6 +19,19 @@ class Reply
     Reply.new(data.first)
   end
 
+  def self.find_by_question_id(id)
+    data = QuestionsDBConnection.instance.execute(<<-SQL, id)
+      SELECT 
+        *
+      FROM
+        replies
+      WHERE 
+        question_id = ?
+    SQL
+
+    data.map { |datum| Reply.new(datum) }
+  end
+
   def initialize(options)
     @id = options['id']
     @parent_reply = options['parent_reply']
