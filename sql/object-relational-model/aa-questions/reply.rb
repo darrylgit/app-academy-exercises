f6d080496d9f364e4c75010ca87586a52c61d2a7
darrylgit <darrylsmithup@gmail.com>
2020-11-25 21:06:44 -0500

    Define Reply::find_by_question_id

--- sql/object-relational-model/aa-questions/reply.rb
+++ sql/object-relational-model/aa-questions/reply.rb
@@ -51,6 +51,21 @@ class Reply
     data.length == 1 ? Reply.new(data.first) : data.map { |datum| Reply.new(datum) }
   end
 
+  def self.find_by_question_id(id)
+    data = QuestionsDBConnection.instance.execute(<<-SQL, id)
+      SELECT 
+        *
+      FROM
+        replies
+      WHERE 
+        question_id = ?
+    SQL
+
+    return "No replies found" if data.length == 0
+
+    data.length == 1 ? Reply.new(data.first) : data.map { |datum| Reply.new(datum) }
+  end
+
   def initialize(options)
     @id = options['id']
     @parent_reply = options['parent_reply']
