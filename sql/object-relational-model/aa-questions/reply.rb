6ca90692e0805f42b3f6339b8473ebba8892914f
darrylgit <darrylsmithup@gmail.com>
2020-11-26 20:19:29 -0500

    Finish rest of basic Reply instance methods

--- sql/object-relational-model/aa-questions/reply.rb
+++ sql/object-relational-model/aa-questions/reply.rb
@@ -79,5 +79,36 @@ class Reply
   end
 
   def question
+    Question.find_by_id(@question_id)
+  end
+
+  def parent_reply
+    data = QuestionsDBConnection.instance.execute(<<-SQL, @parent_reply)
+      SELECT 
+        *
+      FROM
+        replies
+      WHERE 
+        id = @parent_reply
+    SQL
+
+    return "No replies found" if data.length == 0
+
+    Reply.new(data.first)
+  end
+
+  def child_replies
+    data = QuestionsDBConnection.instance.execute(<<-SQL, @id)
+      SELECT 
+        *
+      FROM
+        replies
+      WHERE 
+        parent_reply = @id
+    SQL
+
+    return "No replies found" if data.length == 0
+
+    data.length == 1 ? Reply.new(data.first) : data.map { |datum| Reply.new(datum) }
   end
 end
