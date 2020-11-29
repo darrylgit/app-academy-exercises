22de6c36389ca00ce7a10b2e89597ff96b307314
darrylgit <darrylsmithup@gmail.com>
2020-11-28 21:17:33 -0500

    Define QuestionLike::likers_for_question_id

--- sql/object-relational-model/aa-questions/question_like.rb
+++ sql/object-relational-model/aa-questions/question_like.rb
@@ -19,6 +19,23 @@ class QuestionLike
     QuestionLike.new(data.first)
   end
 
+  def self.likers_for_question_id(id)
+    data = QuestionsDBConnection.instance.execute(<<-SQL, id)
+      SELECT
+        users.id, fname, lname
+      FROM 
+        users
+      JOIN 
+        question_likes ON question_likes.user_id = users.id
+      WHERE
+        question_likes.question_id = ?
+    SQL
+
+    return "No user found" if data.length == 0
+
+    data.length == 1 ? User.new(data.first) : data.map { |datum| User.new(datum) }
+  end
+
   def initialize(options)
     @id = options['id']
     @question_id = options['question_id']
